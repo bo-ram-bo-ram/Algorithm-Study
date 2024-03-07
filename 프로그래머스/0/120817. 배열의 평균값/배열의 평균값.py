8fd49675e1e34aa884049e5f442a50d69e2af00c
boram <128011308+bo-ram-bo-ram@users.noreply.github.com>
2024-03-07 19:35:08 +0900

    [level 0] Title: 배열의 평균값, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,7 +1,2 @@
 def solution(numbers):
-    answer = 0
-    sum = 0 
-    for i in range(1, len(numbers)+1):
-        sum += numbers[i-1]
-    answer = sum / len(numbers)
-    return answer
+    return sum(numbers) / len(numbers)
