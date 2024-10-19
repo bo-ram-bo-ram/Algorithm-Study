8320b4dfc824847a73045e58cac9df2a94b3575f
boram <128011308+bo-ram-bo-ram@users.noreply.github.com>
2024-10-19 23:31:46 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+jumsu = int(input())
+
+if 90<=jumsu <=100 :
+    print("A")
+elif 80<=jumsu <=89 :
+    print("B")
+elif 70<=jumsu <=79 :
+    print("C")
+elif 60<=jumsu <=69 :
+    print("D")
+else :
+    print("F")
+    
